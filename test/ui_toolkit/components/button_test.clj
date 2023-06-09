13cf85c3a6f91c2e9d8ef2240e16185a19cac717
Panthevm <panthevm@yandex.ru>
2023-06-09 18:05:49 +0300

    Add button tests

--- test/ui_toolkit/components/button_test.clj
+++ test/ui_toolkit/components/button_test.clj
@@ -1,10 +1,27 @@
 (ns ui-toolkit.components.button-test
   (:require
-   [ui-toolkit.components.button :as sut]
-   [clojure.test                 :refer [deftest testing]]
-   [matcho.core                  :refer [match]]))
+   [suitkin.toolkit.button :as sut]
+   [clojure.test           :refer [deftest testing]]
+   [matcho.core            :refer [match]]
+   [retest.core            :refer [click datafy]]))
 
-(deftest core-test
-  (match true true)
-  (match true false)
-  )
+(deftest zf-button-test
+  (testing "click"
+    (def counter (atom 0))
+    (click [sut/zf-button {:id ::id :on-click (fn [_] (swap! counter inc))} "text"] ::id)
+    (match @counter 1))
+
+  (testing "loading"
+    (def counter (atom 0))
+    (click [sut/zf-button {:id ::id :loading true :on-click (fn [_] (swap! counter inc))} "text"] ::id)
+    (match @counter 0))
+
+  (testing "disabled"
+    (def counter (atom 0))
+    (click [sut/zf-button {:id ::id :disabled true :on-click (fn [_] (swap! counter inc))} "text"] ::id)
+    (match @counter 0))
+
+  (testing "label"
+    (match
+     (datafy [sut/zf-button {} [:span {:itemProp ::label} "text"]])
+     {::label "text"})))
