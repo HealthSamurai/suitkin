aa2669baea63722e495d07928b5b0b6e12c934f0
islambeg katibov <islambeg@proton.me>
2023-07-07 19:04:53 +0300

    Fix GH Pages build with `cjohansen/portfolio` demo

---
+++ src/ui/build.clj
@@ -0,0 +1,16 @@
+(ns ui.build
+  (:require [optimus.assets :as assets]
+            [optimus.export :as export]))
+
+(def assets
+  (assets/load-assets
+   "public"
+   [#"/portfolio/prism.js"
+    #"/portfolio/styles/portfolio.css"
+    #"/portfolio/canvas.html"]))
+
+(defn export
+  {:shadow.build/stage :flush}
+  [build-state]
+  (export/save-assets assets "_site/assets")
+  build-state)
